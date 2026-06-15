a00f4344daa19e7bcb1ee44620e6218fe77bd3e5
Shubham <shubhamturale.edu@gmail.com>
2026-06-15 16:20:00 +0530

    Day 13

---
+++ day-13-count-for-each/foreach/main.tf
@@ -0,0 +1,16 @@
+variable "env" {
+   type = list(string)
+   default = [ "dev","prod" ]
+  
+}
+
+resource "aws_instance" "name" {
+    ami = "ami-00e801948462f718a"
+    instance_type = "t3.micro"
+    for_each = toset(var.env)
+
+    tags = {
+      Name = each.key
+  
+}
+}
